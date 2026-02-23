7afb62db15be883c4f56d9566d4e85ce6f4397b5
Jason Lo <lcmjlo@gmail.com>
2026-02-22 22:50:48 -0600

    small fixes

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-uv init --bare --build-backend uv --name {{cookiecutter.project_name}}
-
-# add build-backend configuration to pyproject.toml
-if ! grep -qF "[tool.uv.build-backend]" pyproject.toml; then
-    cat <<EOL >> pyproject.toml
-
-[tool.uv.build-backend]
-module-root = "."
-
-[project.scripts]
-{{cookiecutter.package_name}} = "{{cookiecutter.package_name}}.app:main"
-EOL
-fi
+uv init \
+    --description  "{{cookiecutter.description}}" \
+    --build-backend uv
 
 # add dev dependencies to pyproject.toml
 uv add --dev {{cookiecutter._dev_dependencies}}
--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/app.py
+++
@@ -1,6 +0,0 @@
-def main() -> None:
-    print("Hello, {{cookiecutter.project_name}}!")
-
-
-if __name__ == "__main__":
-    main()
